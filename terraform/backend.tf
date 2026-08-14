2a627e0fadcc27248de77d66e07bb27e4d4a7010
SeunDeve <eseun.oyewole@gmail.com>
2026-08-14 03:25:10 +0100

    Fix Terraform formatting

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-backend "s3" {
-bucket = "bedrock-tfstate-REPLACE_WITH_ACCOUNT_ID"
-key = "project-bedrock/terraform.tfstate"
-region = "us-east-1"
-encrypt = true
-use_lockfile = true
-}
+  backend "s3" {
+    bucket       = "bedrock-tfstate-REPLACE_WITH_ACCOUNT_ID"
+    key          = "project-bedrock/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
