c0651a1eb248eb1672500137568817d1936d9963
SeunDeve <eseun.oyewole@gmail.com>
2026-08-14 23:14:43 +0100

    fix: use real account ID for tfstate bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "bedrock-tfstate-REPLACE_WITH_ACCOUNT_ID"
+    bucket       = "bedrock-tfstate-123456789012"
     key          = "project-bedrock/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
