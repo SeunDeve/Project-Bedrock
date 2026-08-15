134204ba70f60602c162f6ee1ebb610c6d5e176f
SeunDeve <eseun.oyewole@gmail.com>
2026-08-15 06:12:54 +0100

    fix: correct tfstate bucket to use real AWS account ID

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "bedrock-tfstate-123456789012"
+    bucket       = "bedrock-tfstate-897722710471"
     key          = "project-bedrock/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
